e7634cb3cea8a6afe290d79f3136da2fff8b1c44
Burkard920 Burkard <stevenburkard24@yahoo.com>
2023-07-12 10:01:11 -0500

    Completed AddEntryForm Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
 
 function App() {
 
   const [entries, setEntries] = useState([{weight: 175, date: '07-11-2023'}, {weight: 176, date: '07-12-2023'}])
 
+  function addNewEntry(entry){
+    let tempEntries = [...entries, entry];
+
+    setEntries(tempEntries);
+  }
+
   return (
     <div>
       <DisplayEntries parentEntries={entries}/>
+      <AddEntryForm addNewEntry={addNewEntry} />
     </div>
   );
 }
